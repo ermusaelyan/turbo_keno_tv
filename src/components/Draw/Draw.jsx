78821dbf0f16c009f9b2924ec06466beb95cbc88
ermusaelyan <e.musaelyan98@gmail.com>
2022-11-01 22:27:09 +0400

    test

--- src/components/Draw/Draw.jsx
+++ src/components/Draw/Draw.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './Draw.module.scss';
 import Loader from '../Loader/Loader';
 import DrawNum from '../DrawNum/DrawNum';
 import BetTemp from '../BetTemp/BetTemp';
+import axios from 'axios';
 
 const Draw = () => {
+  const [drawNums, setDrawNums] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://oliver.energaming.systems:20034/api/v1/get-draw-results`, {
+        params: {
+          token: 'test-frontend-token',
+          duration: 1,
+          page: 0,
+        },
+      })
+      .then(res => setDrawNums(res.data.data.lastRaces[0].r.split(',')));
+  }, []);
   return (
     <div className={s.draw}>
       <div className={s.draw__header}>
@@ -19,11 +32,18 @@ const Draw = () => {
       </div>
       <div className={s.draw__body}>
         <ul className={s.draw__list}>
-          {Array.from(Array(20), (_, i) => (
-            <li key={i} className={s.draw__item}>
-              <DrawNum show={true}>{i + 1}</DrawNum>
-            </li>
-          ))}
+          {drawNums.map(num => {
+            return (
+              <li key={num} className={s.draw__item}>
+                <DrawNum show={false}>{num}</DrawNum>
+              </li>
+            );
+          })}
+          {/*{Array.from(Array(20), (_, i) => (*/}
+          {/*  <li key={i} className={s.draw__item}>*/}
+          {/*    <DrawNum show={true}>{i + 1}</DrawNum>*/}
+          {/*  </li>*/}
+          {/*))}*/}
         </ul>
       </div>
       <div className={s.draw__footer}>
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './History.module.scss';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const History = () => {
+  const [history, setHistory] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://oliver.energaming.systems:20034/api/v1/get-draw-results`, {
+        params: {
+          token: 'test-frontend-token',
+          duration: 1,
+          page: 0,
+        },
+      })
+      .then(res => setHistory(res.data.data.lastRaces.slice(1, 4)));
+  }, []);
   return (
     <div className={s.history}>
       <div className={s.history__title}>Draw History</div>
       <div className={s.history__inner}>
         <ul className={s.history__list}>
-          {Array.from(Array(3), (_, i) => (
-            <li key={i} className={s.history__item}>
-              <div className={s.history__id}>125478</div>
-              <div className={s.historyDraw}>
-                <ul className={s.historyDraw__list}>
-                  {Array.from(Array(20), (_, i) => (
-                    <li key={i} className={s.historyDraw__item}>
-                      {i + 1}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          ))}
+          {history.map(item => {
+            return (
+              <li key={item.id} className={s.history__item}>
+                <div className={s.history__id}>{item.id}</div>
+                <div className={s.historyDraw}>
+                  <ul className={s.historyDraw__list}>
+                    {item.r.split(',').map(num => {
+                      return (
+                        <li key={num} className={s.historyDraw__item}>
+                          {num}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
