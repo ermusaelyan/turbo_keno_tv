428844edbf9037f7e23c70152d873d575d360d02
ermusaelyan <e.musaelyan98@gmail.com>
2022-11-16 00:05:18 +0400

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Game from './components/Game/Game';
 import { useDispatch } from 'react-redux';
 import { getHistory } from './Redux/reducers/historySlice';
+import axios from 'axios';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,6 +17,65 @@ const App = () => {
 
   useEffect(() => {
     loop();
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://oliver.energaming.systems:20034/api/v1/get-draw-results`, {
+        params: {
+          token: 'test-frontend-token',
+          duration: 3,
+          page: 0,
+        },
+      })
+      .then(res => {
+        console.log('Draw Results:', res);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://oliver.energaming.systems:20034/api/v1/get-seconds-till-draw`,
+        {
+          params: {
+            token: 'test-frontend-token',
+            duration: 3,
+            page: 0,
+          },
+        },
+      )
+      .then(res => {
+        console.log('Seconds Till Draw:', res);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://oliver.energaming.systems:20034/api/v1/get-game-versions`, {
+        params: {
+          token: 'test-frontend-token',
+          duration: 3,
+          page: 0,
+        },
+      })
+      .then(res => {
+        console.log('Game Versions:', res);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://oliver.energaming.systems:20034/api/v1/get-draw-history`, {
+        params: {
+          token: 'test-frontend-token',
+          duration: 3,
+          page: 0,
+        },
+      })
+      .then(res => {
+        console.log('Draw History:', res);
+      });
   }, []);
 
   return (
