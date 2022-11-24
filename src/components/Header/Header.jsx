b9dc25a0e4f6014b4e0902b4b535b7f70277c621
ermusaelyan <e.musaelyan98@gmail.com>
2022-11-24 03:45:08 -0800

    upd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,20 +5,22 @@ import { useSelector } from 'react-redux';
 const Header = () => {
   const maxBet = useSelector(state => state.limits.maxBet),
     minBet = useSelector(state => state.limits.minBet),
-    maxWin = useSelector(state => state.limits.maxWin),
-    currency = useSelector(state => state.limits.currency);
+    maxWin = useSelector(state => state.limits.maxWin);
+  // currency = useSelector(state => state.limits.currency);
 
   return (
     <header className={s.header}>
       <div className={s.headerJackpot}>
         <div className={s.headerJackpot__name}>Jackpot</div>
         <div className={s.headerJackpot__sum}>
-          <div className={s.headerJackpot__coin}>520.000.000</div>
+          {/*520.000.000*/}
+          {/*<div className={s.headerJackpot__coin}>Coming Soon</div>*/}
           <div className={s.headerJackpot__currency}>tzs</div>
         </div>
       </div>
       <div className={s.headerWinner}>
-        Last Winner:<span>540 000 {currency}</span>Arusha Marusha
+        {/*Last Winner:<span>540 000 {currency}</span>Arusha Marusha*/}
+        <span>Coming Soon</span>
       </div>
       <div className={s.headerLimits}>
         <div className={s.headerLimits__details}>
